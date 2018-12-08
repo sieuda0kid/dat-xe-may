23361ea4249d1a6f6bd5cbb8450976dee3127760
Vinh1995 <npvinh2303@gmail.com>
2018-12-08 19:59:39 +0700

    update trip Status

--- src/api/controllers/tripController.js
+++ src/api/controllers/tripController.js
@@ -44,29 +44,29 @@ exports.updateTripLocation = function(req,res) {
     })
 }
 
-// exports.updateTripStatus = function(req,res) {
-//     var trip = {
-//         id: req.body.tripId,
-//         status: req.body.tripStatus
-//     }
-//     tripRepo.updateTripStatus(trip)
-//     .then(body => {
-//         var c = {
-//             // address:body.results[0].formatted_address,
-//             // lat:body.results[0].geometry.location.lat,
-//             // lng:body.results[0].geometry.location.lng
-//         }
-//         res.json({
-//             returnCode: 1,
-//             message: "update trip status success!",
-//             object: c
-//         });
-//     })
-//     .catch(err=>{
-//             res.json({
-//             returnCode: 0,
-//             message: "update trip status fail!",
-//             error: err
-//         });
-//     })
-//  }
+exports.updateTripStatus = function(req,res) {
+    var trip = {
+        id: req.body.tripId,
+        status: req.body.tripStatus
+    }
+    tripRepo.updateTripStatus(trip)
+    .then(body => {
+        var c = {
+            // address:body.results[0].formatted_address,
+            // lat:body.results[0].geometry.location.lat,
+            // lng:body.results[0].geometry.location.lng
+        }
+        res.json({
+            returnCode: 1,
+            message: "update trip status success!",
+            object: c
+        });
+    })
+    .catch(err=>{
+            res.json({
+            returnCode: 0,
+            message: "update trip status fail!",
+            error: err
+        });
+    })
+ }
