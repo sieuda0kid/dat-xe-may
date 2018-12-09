530dd2d36d11b1a44bdfa6f3a2d998184e9d30bb
Vinh1995 <npvinh2303@gmail.com>
2018-12-09 20:06:40 +0700

    xu ly socket

--- src/api/app.js
+++ src/api/app.js
@@ -21,6 +21,11 @@ app.use(function (req, res, next) {
     next();
 });
 
+// thiet lap cho router
+app.use('/user', require('./router/userRouter.js'));
+app.use('/refreshToken', require('./router/tokenRouter.js'));
+app.use('/map', require('./router/mapRouter.js'));
+
 app.get('/', (req, res) => {
     res.json({
         msg: 'hello from nodejs api'
@@ -33,3 +38,56 @@ var port = process.env.port || 8888;
 app.listen(port, () => {
     console.log(`api running on port ${port}`);
 })
+
+
+// socket
+var arr=[];
+var io = require('socket.io').listen(server);
+io.on('connection', function (socket) {
+    socket.user={id:0};
+    arr.push(socket);
+    socket.re_status=[{key:0}];
+    console.log('a user connected id= '+socket.id);
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+        arr.splice(arr.indexOf(socket.id),1);
+
+    });
+    socket.on("gui_data",function(data){
+        console.log(data);
+    });
+    socket.on("send_refresh_token",function(data){
+        console.log(data);
+        if(data===null || data.length===0)
+        {
+               console.log("không nhan dc send_refresh_token tu client");
+        }else {
+            token.getUserByRefreshToken(data)
+            .then(rows=>{
+                if(rows.length>0)
+                {
+                    socket.user=rows[0];
+                    //console.log(rows[0]);
+                    socket.re_status.dang_ky_nhan_token=1;
+                }
+            })
+            .catch(error=>{
+                console.log(error);
+            });
+        }   
+    });
+
+})
+// exports.guidata=(data,id)=>{
+// 	 console.log(data);
+// 	// console.log("id="+id);
+// 	// console.log("arr length ="+arr.length);
+
+// 	arr.map(socket=>{
+// 		if(socket.user.id === id && socket.re_status["dang_ky_nhan_token"] === 1)
+// 		{
+//        	    socket.emit("token",data);
+// 		}
+// 	});
+//     }
