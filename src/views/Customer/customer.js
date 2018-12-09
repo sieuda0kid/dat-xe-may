25c2fdfb19304a3d4a159f57d2437bcd3dfe182d
Tai <chitai965123@gmail.com>
2018-12-09 23:33:20 +0700

    Chỉnh sửa giao diện trang Customer

--- src/views/Customer/customer.js
+++ src/views/Customer/customer.js
@@ -5,20 +5,14 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
-import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import { fade } from '@material-ui/core/styles/colorManipulator';
 import { withStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import Button from '@material-ui/core/Button';
-import Icon from '@material-ui/core/Icon';
-
+import NavigationIcon from '@material-ui/icons/Navigation';
 
 
 class Customer extends React.Component {
@@ -122,7 +116,7 @@ class Customer extends React.Component {
            
             <Button variant="contained"  className={classes.button}>
                 Đặt Xe
-                <Icon className={classes.rightIcon}>send</Icon>
+                <NavigationIcon className={classes.extendedIcon} />
             </Button>
             </div>
             <div className={classes.sectionMobile}>
