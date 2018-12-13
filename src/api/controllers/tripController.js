8bfbf824333f86fb0acdcd56ae40d034aaacc666
Vinh1995 <npvinh2303@gmail.com>
2018-12-13 22:02:28 +0700

    update lai ham triplocation

--- src/api/controllers/tripController.js
+++ src/api/controllers/tripController.js
@@ -1,47 +1,24 @@
 var tripRepo = require('../repo/TripRepos.js');
 var mapRepo = require('../repo/MapRepos.js');
 require('dotenv').config();
-
+var moment=require('moment');
+var socket=require('../app.js');
 exports.updateTripLocation = function(req,res) {
-   // var stringUrl=`https://maps.googleapis.com/maps/api/geocode/json?latlng=${req.body.tripLat},${req.body.tripLong}&key=${process.env.key}`;
-  //  var url = encodeURI(stringUrl);
-    mapRepo.getMapAPI(url)
-    .then(body => {
-        var trip = {
-            id: req.body.tripId,
-            tripLocation: body.results[0].formatted_address,
-            tripLat: body.results[0].geometry.location.lat,
-            tripLong: body.results[0].geometry.location.lng
-        };
-
-        tripRepo.updateTripLocation(trip)
-        .then(body => {
-            var c = {
-                // address:body.results[0].formatted_address,
-                // lat:body.results[0].geometry.location.lat,
-                // lng:body.results[0].geometry.location.lng
-            }
-            res.json({
-                returnCode: 1,
-                message: "update trip location success!",
-                object: c
-            });
-        })
-        .catch(err=>{
-                res.json({
-                returnCode: 0,
-                message: "update trip location fail!",
-                error: err
-            });
-        })
+    var c=req.body;
+    tripRepo.updateTripLocation(c)
+    .then(rows=>{
+        res.json({
+                returnCode:1,
+                message:"update TripLocation thanh công!",
+    })
     })
     .catch(err=>{
-            res.json({
+        res.json({
                 returnCode:0,
-                message:"get Lat Lng error!",
+                message:"update TripLocation không thành công!",
                 error:err
             });
-    })
+    });
 }
 
 exports.getAllTrip=function(req,res){
