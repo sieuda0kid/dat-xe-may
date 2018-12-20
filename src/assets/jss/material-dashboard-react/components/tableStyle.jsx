9aeb4eda593b8ede632037bc6507715f71fe2cc8
Tran Dat <912.tranminhdat@gmail.com>
2018-12-20 13:08:37 +0700

    thêm các tài nguyên có sẵn của material-ui

---
+++ src/assets/jss/material-dashboard-react/components/tableStyle.jsx
@@ -0,0 +1,71 @@
+import {
+  warningColor,
+  primaryColor,
+  dangerColor,
+  successColor,
+  infoColor,
+  roseColor,
+  grayColor,
+  defaultFont
+} from "../../material-dashboard-react.jsx";
+
+const tableStyle = theme => ({
+  warningTableHeader: {
+    color: warningColor
+  },
+  primaryTableHeader: {
+    color: primaryColor
+  },
+  dangerTableHeader: {
+    color: dangerColor
+  },
+  successTableHeader: {
+    color: successColor
+  },
+  infoTableHeader: {
+    color: infoColor
+  },
+  roseTableHeader: {
+    color: roseColor
+  },
+  grayTableHeader: {
+    color: grayColor
+  },
+  table: {
+    marginBottom: "0",
+    width: "100%",
+    maxWidth: "100%",
+    backgroundColor: "transparent",
+    borderSpacing: "0",
+    borderCollapse: "collapse"
+  },
+  tableHeadCell: {
+    color: "inherit",
+    ...defaultFont,
+    fontSize: "1em"
+  },
+  tableCell: {
+    ...defaultFont,
+    lineHeight: "1.42857143",
+    padding: "12px 8px",
+    verticalAlign: "middle"
+  },
+  tableResponsive: {
+    width: "100%",
+    overflowX: "auto"
+  },
+  tablePagination:{
+    flex: '1 1 50%',
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none"
+  }
+});
+
+export default tableStyle;
