933b3c7db9a1aad85fd2439ba2c80f8092e393f5
Vinh1995 <npvinh2303@gmail.com>
2018-12-20 20:04:47 +0700

    chinh sua trong triprouter

--- src/api/router/tripRouter.js
+++ src/api/router/tripRouter.js
@@ -4,4 +4,16 @@ var token=require('../controller/tokenController.js');
 
 var router = express.Router();
 
- 
+router.route('/getAllTrip')
+.post(token.checkAccessToken,tripCtrl.getAllTrip);
+
+router.route('/addCustomerAndTrip')
+.post(token.checkAccessToken,tripCtrl.addCustomerAndTrip);
+module.exports = router;
+
+
+router.route('/getTripByDriverId')
+.post(token.checkAccessToken,tripCtrl.getTripByDriverId);
+
+router.route('/updateTripStatus')
+.post(token.checkAccessToken,tripCtrl.updateTripStatus);
