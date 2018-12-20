f687cf13643189df4e5112a4226e4b007753a201
Vinh1995 <npvinh2303@gmail.com>
2018-12-20 20:00:35 +0700

    chinh trong file tripRepo.js them truy van

--- src/api/repo/tripRepo.js
+++ src/api/repo/tripRepo.js
@@ -1,11 +1,51 @@
 var db = require('../fn/mysql-db');
 
 exports.updateTripLocation = function(trip) {
-	var sql = `update trip set tripLocation = '${trip.tripLocation}', tripLatitude = '${trip.tripLat}', tripLongitude = '${trip.tripLong}' where id = '${trip.id}'`;
+	var sql = `update chuyen set tripLocation = '${trip.tripLocation}', tripLatitude = '${trip.tripLat}', tripLongitude = '${trip.tripLong}' where id = '${trip.id}'`;
 	return db.write(sql);
 }
 
 exports.updateTripStatus = function(trip) {
-	var sql = `update trip set status = '${trip.status}' where id = '${trip.id}'`;
+	var sql = `update chuyen set status = '${trip.status}' where id = '${trip.id}'`;
 	return db.write(sql);
 }
+
+
+// exports.addCustomer=function(cus){
+// 	var sql =  `insert into khachhang(customerName,customerPhone,customerAddress,isDelete) values('${cus.customerName}','${cus.customerPhone}','${cus.customerAddress}',${cus.isDelete})`;
+// 	console.log(sql);
+// 	return db.write(sql);
+// }
+
+// exports.loadTrip=function(cus){
+// 	var sql = `select * from chuyen ORDER BY requestTime DESC`;
+// 	console.log(sql);
+// 	return db.write(sql);
+// }
+
+// exports.loadTripFull=function(cus){
+// 	var sql = `select * from trip t,customer c,staff s where t.customerId = c.id and t.driverId=s.id ORDER BY requestTime DESC`;
+// 	console.log(sql);
+// 	return db.write(sql);
+// }
+// exports.loadTripFull2=function(cus){
+// 	var sql = `select t.*, ta.id as statusId, ta.statusName, s.fullname as driverName, c.id as customerId, c.customerName, c.customerPhone, c.customerAddress from trip t INNER JOIN customer c ON t.customerId = c.id LEFT JOIN tripstatus ta ON t.status = ta.id LEFT JOIN staff s ON t.driverId=s.id ORDER BY t.requestTime DESC`; 
+// 	console.log(sql);
+// 	return db.write(sql);
+// }
+// exports.getTripByDriverId=function(id){
+//     var sql = `select t.*, ta.id as statusId, ta.statusName, s.fullname as driverName, c.id as customerId, c.customerName, c.customerPhone, c.customerAddress from trip t INNER JOIN customer c ON t.customerId = c.id LEFT JOIN tripstatus ta ON t.status = ta.id LEFT JOIN staff s ON t.driverId=s.id where t.driverID = ${id}`;
+// 	console.log(sql);
+// 	return db.write(sql);
+// }
+// exports.getTripByTripId=function(id){
+//     var sql = `select t.*, ta.id as statusId, ta.statusName, s.fullname as driverName, c.id as customerId, c.customerName, c.customerPhone, c.customerAddress from trip t INNER JOIN customer c ON t.customerId = c.id LEFT JOIN tripstatus ta ON t.status = ta.id LEFT JOIN staff s ON t.driverId=s.id where t.id = ${id}`;
+// 	console.log(sql);
+// 	return db.write(sql);
+// }
+// exports.addTrip=function(trip){
+// 	var sql =  `insert into trip(customerId,driverId,tripLocation,tripLongitude,tripLatitude,status,note,requestTime,isDelete) 
+// 	values(${trip.customerId},${trip.driverId},'${trip.tripLocation}','${trip.tripLongitude}','${trip.tripLatitude}',${trip.status},'${trip.note}',${trip.requestTime},${trip.isDelete})`;
+// 	console.log(sql);
+// 	return db.write(sql);
+// }
--- src/api/repo/userRepo.js
+++ src/api/repo/userRepo.js
@@ -1,46 +1,67 @@
-var db  = require('../fn/mysql-db');
-var md5 = require('crypto-js/md5');
+// var db  = require('../fn/mysql-db');
+// var md5 = require('crypto-js/md5');
 
+// exports.loadAll = function() {
+// 	var sql = `select * from staff where isDelete = 0`;
+// 	console.log(sql);
+// 	return db.load(sql);
+// }
 
-exports.loadAll = function() {
-	 
-}
-
-
-exports.loadOne = function(id) {
-	 
-}
-
-
-exports.loadOnebyType = function(type) {
+// exports.loadOne = function(id) {
+// 	// console.log("id load one :"+id);
+// 	var sql = `select * from staff where isDelete = 0 and id = ${id}`;
+// 	console.log(sql);
+// 	return db.load(sql);
+// }
+// exports.loadOnebyType = function(type) {
 	
-	 
-}
-
-exports.getUserByRefreshToken = function(reToken) {
- 
-}
-
-
-exports.login = (u) => {
-    var sql = `select * from account where username = '${u.username}' and password = '${u.password}' and type = '${u.type}'`;
-	console.log(sql);
-	return db.load(sql);
-}
-
-
-exports.loadForType = function(difriend) {
- 
+// 	var sql = `select * from staff where isDelete = 0 and userType = ${type}`;
+// 	console.log(sql);
+// 	return db.load(sql);
+// }
+// exports.loadForType = function(difriend) {
+// 	if(difriend===0)
+// 	{
+// 		var sql = `select * from staff where isDelete = 0 and userType = 4`;
+// 	    console.log(sql);
+// 	    return db.load(sql);
+// 	}else{
+// 		var sql = `select * from staff where isDelete = 0 and userType != 4`;
+// 	   console.log(sql);
+// 	    return db.load(sql);
+// 	}
 	
-}
+// }
+// exports.getUserByRefreshToken = function(reToken) {
+// 	var sql = `select * from staff st ,token tk where tk.refresh_token = '${reToken}' and st.id=tk.id_user`;
+// 	console.log(sql);
+// 	return db.load(sql);
+// }
+// exports.login = function(c) {
+// 	var sql = `select * from staff where username = '${c.username}' and password = '${c.password}'`;
+// 	console.log(sql);
+// 	return db.load(sql);
+// }
 
+// exports.add = function(c) {
+// 	var sql = `insert into staff(username,password,fullname,userType,isDelete,phone,dob) values('${c.username}','${c.password}','${c.fullname}',${c.userType},${c.isDelete},'${c.phone}','${c.dob}')`;
+// 	console.log(sql);
+// 	return db.write(sql);
+// }
 
-exports.add = function(c) {
-	var sql = `insert into staff(username,password,fullname,userType,isDelete,phone,dob) values('${c.username}','${c.password}','${c.fullname}',${c.userType},${c.isDelete},'${c.phone}','${c.dob}')`;
-	return db.write(sql);
-}
-
-exports.delete = function(id) {
-	var sql = `delete from staff where id = ${id}`;
-	return db.write(sql);
-}
+// exports.delete = function(id) {
+// 	var sql = `delete from staff where id = ${id}`;
+// 	console.log(sql);
+// 	return db.write(sql);
+// }
+// exports.loadDriver = function(id) {
+// 	var sql = `delete from staff where id = ${id}`;
+// 	console.log(sql);
+// 	return db.write(sql);
+// }
+// exports.addDriver=function(driver)
+// {
+// 	var sql = `insert into driver(staffId,status) values('${driver.staffId}',${driver.status})`;
+// 	console.log(sql);
+// 	return db.write(sql);
+// }
